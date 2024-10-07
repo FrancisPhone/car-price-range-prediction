eff88a5fb371af28576a7ae762ec7754059dca27
Phone Myint Naing <francisphone1998@gmail.com>
2024-10-07 13:45:07 +0700

    test main.yml

--- production.py
+++ production.py
@@ -1,18 +1,24 @@
 import mlflow
-import pickle
 import os
+from dotenv import load_dotenv
 
-tracking_uri = "https://mlflow.ml.brain.cs.ait.ac.th/"
+load_dotenv()
+
+tracking_uri = os.getenv('MLFLOW_TRACKING_URI')
 mlflow.set_tracking_uri(tracking_uri)
-os.environ['MLFLOW_TRACKING_USERNAME'] = 'admin'
-os.environ['MLFLOW_TRACKING_PASSWORD'] = 'password'
-os.environ["LOGNAME"] = "st124973"
-stage = 'Production'
-model_name = 'st124973-a3-model'
+os.environ['MLFLOW_TRACKING_USERNAME'] = os.getenv('MLFLOW_TRACKING_USERNAME')
+os.environ['MLFLOW_TRACKING_PASSWORD'] = os.getenv('MLFLOW_TRACKING_PASSWORD')
+os.environ["LOGNAME"] = os.getenv('LOGNAME')
+stage = os.getenv('stage')
+model_name = os.getenv('APP_MODEL_NAME')
 
 client = mlflow.tracking.MlflowClient()
 client.transition_model_version_stage(name=model_name, version=1, stage=stage)
 
 model = mlflow.sklearn.load_model(model_uri=f"models:/{model_name}/{stage}")
-with open('st124973-a3-model', 'wb') as handle:
-    pickle.dump(model, handle, protocol=pickle.HIGHEST_PROTOCOL)
+if not os.path.exists(model_name):
+    mlflow.sklearn.save_model(
+        model,
+        model_name,
+        serialization_format=mlflow.sklearn.SERIALIZATION_FORMAT_CLOUDPICKLE,
+    )
